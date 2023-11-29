f9aa17c174ccb86b2462092485f3f4b05f435a16
山田 大貴 <yamada_daiki@soft-com.co.jp>
2023-11-29 13:08:17 +0900

    support next ssg

--- node/server.js
+++ node/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+app.use(express.static('out'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('/out/index.html')
 })
 
 app.listen(port, () => {
